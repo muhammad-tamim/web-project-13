5173b2180248118e8432432081c5b2d6d1aa00ee
muhammad-tamim <contact2tamim@gmail.com>
2025-08-30 04:08:14 +0600

    refactor: make the js code little bit cliner

--- script/script.js
+++ script/script.js
@@ -3,148 +3,125 @@ const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
 const cardList = document.querySelector(".card-list");
 const cartList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".cart-total");
 
-
-// open cart tab
-cartIcon.addEventListener("click", () => {
-    cartTab.classList.add("cart-tab-active");
-
-})
-
-// close cart tab
-closeBtn.addEventListener("click", () => {
-    cartTab.classList.remove("cart-tab-active")
-})
-
-
-// menu cards
+cartIcon.addEventListener("click", () => cartTab.classList.add("cart-tab-active"));
+closeBtn.addEventListener("click", () => cartTab.classList.remove("cart-tab-active"));
 
 let productList = [];
+let cartProduct = [];
 
-const fetchProduct = () => {
-    fetch("../assets/products.json")
-        .then(res => res.json())
-        .then(data => {
-            productList = data
-            showCards()
-        });
+const updateTotals = () => {
+
+
+    let totalPrice = 0;
+
+    document.querySelectorAll(".item").forEach(item => {
+        const price = parseFloat(item.querySelector(".item-total").textContent.replace("$", ""));
+
+        totalPrice += price;
+    })
+    cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+
 }
 
-fetchProduct()
-
 const showCards = () => {
-    productList.forEach((product) => {
+    productList.forEach(product => {
         const div = document.createElement("div");
         div.classList.add("order-card");
-
         div.innerHTML = `
         <div class="card-image">
             <img src=${product.image} alt="">
         </div>
         <h4>${product.name}</h4>
         <h4 class="price">${product.price}</h4>
-        <a class="btn cart-btn" href="#">Add To Cart</a>
+        <a class="btn card-btn" href="#">Add To Cart</a>
         `
         cardList.appendChild(div);
 
-        const cartBtn = div.querySelector(".cart-btn");
-        cartBtn.addEventListener("click", (e) => {
-            e.preventDefault()
+        const cardBtn = div.querySelector(".card-btn");
+        cardBtn.addEventListener("click", (e) => {
+            e.preventDefault();
             addToCart(product)
         })
     })
 }
 
-/*
-OR
-const showCards = () => {
-    productList.forEach((product) => {
-        cardList.innerHTML += `
-            <div class="order-card">
-                        <div class="card-image">
-                            <img src=${product.image} alt="">
-                        </div>
-                        <h4>${product.name}</h4>
-                        <h4 class="price">${product.price}</h4>
-                        <a class="btn menu-btn" href="#">Add To Cart</a>
-                    </div>
-        `
-    })
-}
-
-*/
-
-// cart tab
-
-let cartProduct = []
-let quantity = 1;
-let price = parseFloat(product.price.replace("$", ""))
-
 const addToCart = (product) => {
 
-    const existingProduct = cartProduct.find((item) => item.id === product.id);
+    const existingProduct = cartProduct.find(item => item.id === product.id);
 
     if (existingProduct) {
-        alert("Item already in you'r cart");
+        alert("Item already in your cart");
         return;
     }
+    cartProduct.push(product);
+    let quantity = 1;
+    let price = parseFloat(product.price.replace("$", ""));
 
-    cartProduct.push(product)
+    const div = document.createElement("div");
+    div.classList.add("item");
+    div.innerHTML = `
+                <div class="item-image">
+                    <img src=${product.image} alt="">
+                </div>
+                <div class="detail">
+                    <h4>${product.name}</h4>
+                    <h4 class="item-total">${product.price}</h4>
+                </div>
+                <div class="flex">
+                    <a href="#" class="quantity-btn minus">
+                        <i class="fa-solid fa-minus"></i>
+                    </a>
+                    <h4 class="quantity-value">${quantity}</h4>
+                    <a href="#" class="quantity-btn plus">
+                        <i class="fa-solid fa-plus"></i>
+                    </a>
+                </div>
+                `;
+    cartList.appendChild(div);
+    updateTotals();
 
-    const cartItem = document.createElement("div");
-    cartItem.classList.add("item");
+    const plusBtn = div.querySelector(".plus");
+    const minusBtn = div.querySelector(".minus");
 
-    cartItem.innerHTML = `
-    <div class="item-image">
-        <img src=${product.image} alt="">
-    </div>
-        
-    <div class="detail">
-        <h4>${product.name}</h4>
-        <h4 class="item-total">${product.price}</h4>
-    </div>
-    
-    <div class="flex">
-        <a href="#" class="quantity-btn minus">
-            <i class="fa-solid fa-minus"></i>
-        </a>
-        <h4 class="quantity-value">${quantity}</h4>
-        <a href="#" class="quantity-btn plus">
-            <i class="fa-solid fa-plus"></i>
-        </a>
-    </div>
-    `
-    cartList.appendChild(cartItem);
-
-
-    // handle item increment and decrement
-    const plusBtn = document.querySelector(".plus")
-    const minusBtn = document.querySelector(".minus")
-    const quantityValue = cartItem.querySelector(".quantity-value");
-    const itemTotal = cartItem.querySelector(".item-total");
+    const quantityValue = div.querySelector(".quantity-value");
+    const itemTotal = document.querySelector(".item-total");
 
     plusBtn.addEventListener("click", (e) => {
         e.preventDefault()
-
         quantity++;
+
         quantityValue.textContent = quantity;
-        itemTotal.textContent = `$${(price * quantity).toFixed(2)}`
+        itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+        updateTotals()
     })
     minusBtn.addEventListener("click", (e) => {
         e.preventDefault()
-
         if (quantity > 1) {
-
             quantity--;
             quantityValue.textContent = quantity;
-            itemTotal.textContent = `$${(price * quantity).toFixed(2)}`
+            itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+            updateTotals()
         }
         else {
-            cartItem.cardList.add("slide-out")
+            div.classList.add("slide-out");
             setTimeout(() => {
-                cartItem.remove;
-                cartProduct = cartProduct.filter(item => item.id !== product.id)
+                div.remove();
+                cartProduct = cartProduct.filter(item => item.id !== product.id);
+                updateTotals()
             }, 300);
         }
     })
 }
+
+const initApp = () => {
+    fetch("../assets/products.json")
+        .then(res => res.json())
+        .then(data => {
+            productList = data;
+            showCards();
+        })
+}
+
+initApp();
