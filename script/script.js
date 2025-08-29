b644c637c2455768a8e8f297e5630296ed8f45bc
muhammad-tamim <contact2tamim@gmail.com>
2025-08-30 00:15:30 +0600

    feat: show all menu cards using js

--- script/script.js
+++ script/script.js
@@ -15,3 +15,54 @@ closeBtn.addEventListener("click", () => {
 })
 
 
+// menu cards
+
+let productList = [];
+
+const fetchProduct = () => {
+    fetch("../assets/products.json")
+        .then(res => res.json())
+        .then(data => {
+            productList = data
+            showCards()
+        });
+}
+
+fetchProduct()
+
+const showCards = () => {
+    productList.forEach((product) => {
+        const div = document.createElement("div");
+        div.classList.add("order-card");
+
+        div.innerHTML = `
+        <div class="card-image">
+            <img src=${product.image} alt="">
+        </div>
+        <h4>${product.name}</h4>
+        <h4 class="price">${product.price}</h4>
+        <a class="btn" href="#">Add To Cart</a>
+        `
+
+        cartList.appendChild(div);
+    })
+}
+
+/*
+OR
+const showCards = () => {
+    productList.forEach((product) => {
+        cartList.innerHTML += `
+            <div class="order-card">
+                        <div class="card-image">
+                            <img src=${product.image} alt="">
+                        </div>
+                        <h4>${product.name}</h4>
+                        <h4 class="price">${product.price}</h4>
+                        <a class="btn" href="#">Add To Cart</a>
+                    </div>
+        `
+    })
+}
+
+*/
