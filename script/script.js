03d58750972f95054f18ffe7948b04644464fdf4
muhammad-tamim <contact2tamim@gmail.com>
2025-08-30 00:53:11 +0600

    feat: add item to the add to cart tab

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 const cartIcon = document.querySelector(".cart-icon");
 const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
-const cartList = document.querySelector(".card-list");
+const cardList = document.querySelector(".card-list");
+const cartList = document.querySelector(".cart-list");
+
 
 // open cart tab
 cartIcon.addEventListener("click", () => {
@@ -41,10 +43,15 @@ const showCards = () => {
         </div>
         <h4>${product.name}</h4>
         <h4 class="price">${product.price}</h4>
-        <a class="btn" href="#">Add To Cart</a>
+        <a class="btn cart-btn" href="#">Add To Cart</a>
         `
+        cardList.appendChild(div);
 
-        cartList.appendChild(div);
+        const cartBtn = div.querySelector(".cart-btn");
+        cartBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            addToCart(product)
+        })
     })
 }
 
@@ -52,17 +59,59 @@ const showCards = () => {
 OR
 const showCards = () => {
     productList.forEach((product) => {
-        cartList.innerHTML += `
+        cardList.innerHTML += `
             <div class="order-card">
                         <div class="card-image">
                             <img src=${product.image} alt="">
                         </div>
                         <h4>${product.name}</h4>
                         <h4 class="price">${product.price}</h4>
-                        <a class="btn" href="#">Add To Cart</a>
+                        <a class="btn menu-btn" href="#">Add To Cart</a>
                     </div>
         `
     })
 }
 
 */
+
+// cart tab
+
+let cartProduct = []
+
+const addToCart = (product) => {
+
+    const existingProduct = cartProduct.find((item) => item.id === product.id);
+
+    if (existingProduct) {
+        alert("Item already in you'r cart");
+        return;
+    }
+
+    cartProduct.push(product)
+
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("item");
+
+    cartItem.innerHTML = `
+    <div class="item-image">
+        <img src=${product.image} alt="">
+    </div>
+        
+    <div class="detail">
+        <h4>${product.name}</h4>
+        <h4 class="item-total">${product.price}</h4>
+    </div>
+    
+    <div class="flex">
+        <a href="#" class="quantity-btn minus">
+            <i class="fa-solid fa-minus"></i>
+        </a>
+        <h4 class="quantity-value">1</h4>
+        <a href="#" class="quantity-btn plus">
+            <i class="fa-solid fa-plus"></i>
+        </a>
+    </div>
+    `
+    cartList.appendChild(cartItem);
+
+}
